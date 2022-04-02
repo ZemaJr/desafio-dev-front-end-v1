37becdc401d3617d6d4099d2315786dece1102ab
José de Castro Jr <66797597+ZemaJr@users.noreply.github.com>
2022-04-02 12:12:11 -0300

    V020420221212

--- src/app/features/users/users.service.ts
+++ src/app/features/users/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loadUsers(): void {
+    this.http.get('https://reqres.in/api/users')
+  }
+
 }
